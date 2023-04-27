349dfd1663e626150d99d88f2bb8e0e9d28f1cb3
quanKM <quankm.it@gmail.com>
2023-04-28 00:12:19 +0700

    feat: add projects

---
+++ components/home/product/index.tsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+import Flicking from "@egjs/react-flicking";
+import "@egjs/react-flicking/dist/flicking.css";
+const posts = [
+  {
+    title: "Leazo",
+    href: "http://leazo.vn/",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "E-Commerce", href: "#" },
+    image: require("../../../assets/projects/leazo.png"),
+  },
+  {
+    title: "VicPartners",
+    href: "https://vic.partners/",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Marketing", href: "#" },
+    image: require("../../../assets/projects/vic-partners.png"),
+  },
+  {
+    title: "CruiseBook",
+    href: "https://www.cruise-book.com/",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    category: { title: "Marketing", href: "#" },
+    image: require("../../../assets/projects/cruise-book.png"),
+  },
+];
+
+export default function Product() {
+  return (
+    <div className="py-24 bg-white sm:py-32">
+      <div className="px-6 mx-auto max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            What we do
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+        </div>
+        <div className="grid max-w-2xl grid-cols-1 pt-10 mx-auto mt-10 border-t border-gray-200 gap-y-16 gap-x-8 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post, index) => (
+            <article
+              key={String(index)}
+              className="flex flex-col items-start justify-between max-w-xl"
+            >
+              <div className="flex items-center text-xs gap-x-4">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.category.href}
+                  className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="relative group">
+                <div className="mt-3 lg:h-60 md:h-48">
+                  <Image
+                    src={post.image}
+                    width={400}
+                    alt="Post image"
+                    className="object-cover aspect-[4/3]  w-full h-full transition ease-in-out delay-150 cursor-pointer hover:scale-110"
+                  />
+                </div>
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">
+                  {post.description}
+                </p>
+              </div>
+              {/* <div className="relative flex items-center mt-8 gap-x-4">
+                <img
+                  src={post.author.imageUrl}
+                  alt=""
+                  className="w-10 h-10 rounded-full bg-gray-50"
+                />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div> */}
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
