381e06a797fa5d33b0b49b83b78ec426fe837e17
quanKM <quankm.it@gmail.com>
2023-04-28 22:34:42 +0700

    fix: confict

--- components/projects/MainNav/index.tsx
+++ components/projects/MainNav/index.tsx
@@ -59,11 +59,7 @@ export default function MainNav() {
               <CogIcon className="w-6 h-6" aria-hidden="true" />
             </Popover.Button>
           </div>
-<<<<<<<< HEAD:layouts/MainNav/index.tsx
           <Popover.Group as="nav" className="hidden ml-0 space-x-10 md:flex">
-========
-          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
->>>>>>>> origin:components/projects/MainNav/index.tsx
             {/* <Popover className="relative">
               {({ open }) => (
                 <>
@@ -127,16 +123,6 @@ export default function MainNav() {
 
             <Link
               href={"/projects"}
-<<<<<<<< HEAD:layouts/MainNav/index.tsx
-              className="text-base font-medium text-gray-500 hover:text-gray-900 "
-            >
-              Projects
-            </Link>
-            <Link
-              href={"/about-us"}
-              className="text-base font-medium text-gray-500 hover:text-gray-900"
-            >
-========
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
               Projects
@@ -145,7 +131,6 @@ export default function MainNav() {
               href={"/about-us"}
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
->>>>>>>> origin:components/projects/MainNav/index.tsx
               About Us
             </Link>
           </Popover.Group>
